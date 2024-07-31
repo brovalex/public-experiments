c493ec5bde74c981d55ab82de2ac786493bddec4
Alex Brovkin <alex@webmovement.com>
2024-07-30 22:36:58 -0400

    first working api seed python

---
+++ receipts/receipts-app/src/app/api/referenceItem/route.js
@@ -0,0 +1,28 @@
+// app/api/reference-item/route.js
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function POST(req) {
+  try {
+    const { name, quantity, unitOfMeasure, price, pricePerWeight, referenceUrl } = await req.json();
+
+    // Create a new ReferenceItem
+    const newReferenceItem = await prisma.referenceItem.create({
+      data: {
+        name,
+        quantity,
+        unitOfMeasure,
+        price,
+        pricePerWeight,
+        referenceUrl,
+      },
+    });
+
+    return new Response(JSON.stringify(newReferenceItem), { status: 200 });
+  } catch (error) {
+    console.error('Error creating reference item:', error);
+    return new Response(JSON.stringify({ error: 'Failed to create reference item '+error }), { status: 500 });
+  }
+}
