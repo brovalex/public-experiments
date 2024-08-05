ef9f56db4e71575898c43090b2a361a9b411492c
Alex Brovkin <alex@webmovement.com>
2024-08-05 09:08:16 -0400

    fixed typo

--- receipts/receipts-app/src/types/receipt.d.ts
+++ receipts/receipts-app/src/types/receipt.d.ts
@@ -2,5 +2,5 @@ import { ImageFile, Expense, Receipt as PrismaReceipt } from '@prisma/client';
 
 export interface ReceiptWithRelationships extends PrismaReceipt {
   imageFiles: ImageFile[];
-  expenses: Exepnse[];
+  expenses: Expense[];
 }
