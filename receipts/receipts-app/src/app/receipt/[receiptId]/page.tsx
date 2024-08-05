29785945ca8ef6348331f92ec8c44e2512d97c80
Alex Brovkin <alex@webmovement.com>
2024-08-05 09:08:28 -0400

    implemented table view

--- receipts/receipts-app/src/app/receipt/[receiptId]/page.tsx
+++ receipts/receipts-app/src/app/receipt/[receiptId]/page.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
 import { Expense as PrismaExpense } from '@prisma/client';
 import { ReceiptWithRelationships } from '@/types/receipt.d';
+import { Table } from "flowbite-react";
 
 const ReceiptPage = () => {
     const params = useParams();
@@ -34,22 +35,37 @@ const ReceiptPage = () => {
                 )}
             </div>
             <div className="w-1/2 p-4">
-                <h1 className="text-2xl font-bold">Receipt</h1>
-                <p>Receipt ID: {receiptId}</p>
-                <hr />
-                {expenses.length > 0 ? (
-                    <ul>
-                        {expenses.map((expense: PrismaExpense) => (
-                            <li key={expense.id}>
-                                <p>{expense.referenceItem.name}</p>
-                                <code>{expense.receiptText.text}</code>
-                                <p>{expense.priceEach} x {expense.quantity}</p>
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>No expenses on this receipt. </p>
-                )}
+                <Table>
+                    <Table.Head>
+                    <Table.HeadCell>Item</Table.HeadCell>
+                    <Table.HeadCell>Quantity</Table.HeadCell>
+                    <Table.HeadCell>Price Each</Table.HeadCell>
+                    <Table.HeadCell>
+                        <span className="sr-only">Edit</span>
+                    </Table.HeadCell>
+                    </Table.Head>
+                    {expenses.length > 0 ? (
+                    <Table.Body className="divide-y">
+                    {expenses.map((expense: PrismaExpense) => (
+                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800" key={expense.id}>
+                            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                <span>Product name not implement yet</span>
+                                <p className="font-normal text-xs text-slate-400">{expense.referenceItem.name}</p>
+                            </Table.Cell>
+                            <Table.Cell>{expense.quantity} ×</Table.Cell>
+                            <Table.Cell>{expense.priceEach}</Table.Cell>
+                            <Table.Cell>
+                            <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                                Edit
+                            </a>
+                            </Table.Cell>
+                        </Table.Row>
+                    ))}
+                    </Table.Body>
+                    ) : (
+                        <p>No expenses on this receipt. </p>
+                    )}
+                </Table>
                 {/* <pre>{JSON.stringify(receipt, null, 2)}</pre> */}
             </div>
         </div>
